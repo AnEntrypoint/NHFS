baa744961da5144448692b9993058010cf07ac63
Ali Sanan <107338507+AliSananS@users.noreply.github.com>
2025-08-11 00:32:11 +0500

    Added fileContext hook in TODO

---
+++ hooks/useFilesDataContext.ts
@@ -0,0 +1,3 @@
+/* TODO: Create a files data context to keep track of the files in current directory.
+Use it for, Copying, Rename and Uploading files as old way of trusting `parhname` is very error prone and broken.
+*/
